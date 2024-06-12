74306ca66c5497b822b16311ef0df6a10aa8f831
Sajika2000 <95673193+Sajika2000@users.noreply.github.com>
2024-06-12 18:00:52 +0530

    Add Login signUp page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login_SignUp from './Components/Login_SignUp/Login_SignUp';
+
+
+
+function App() {
+  return (
+    <div>
+         <Login_SignUp/>
+      
+    </div>
+  );
+}
+
+export default App;
